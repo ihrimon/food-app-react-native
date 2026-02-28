e6c681aad34a109e84bdc9209c5f0c0f8cdb7f08
ihrimon <ihrimon147@gmail.com>
2026-02-28 19:59:51 +0600

    sign-up and sign-in screen created

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: 'com.ihrimon.food-app',
+  databaseId: '69a2e7e800138330d749',
+  userCollectionId: 'user',
+};
